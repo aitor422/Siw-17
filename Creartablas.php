8c969ce4103b3c44c4eb40d973906945482bac57
MikelArdanaz <mikelillo_1@msn.com>
2017-04-12 11:45:25 +0200

    Añadido Script para crear tablas de la base de datos

--- Creartablas.php
+++ Creartablas.php
@@ -1,100 +1,108 @@
 <?php
-   $dblink = mysqli_connect("http://webalumnos.tlm.unavarra.es:10298/", "siw14", "eeshaekaip", "db_siw14");
+   $conn = mysqli_connect("dbserver", "siw14", "eeshaekaip", "db_siw14");
    if (mysqli_connect_errno()){
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
    }
    $sql="-- -----------------------------------------------------
-   -- Table `mydb`.`productos`
+   -- Table productos
    -- -----------------------------------------------------
-   CREATE TABLE IF NOT EXISTS `mydb`.`productos` (
-     `idproducto` INT NOT NULL,
-     `categoria` VARCHAR(45) NULL,
-     `nombre` VARCHAR(45) NULL,
-     `precio` INT NULL,
-     `destacado` TINYINT(1) NULL,
-     PRIMARY KEY (`idproducto`))
-   ENGINE = InnoDB;
+   CREATE TABLE IF NOT EXISTS productos (
+     idproducto INT NOT NULL,
+     categoria VARCHAR(45) NULL,
+     nombre VARCHAR(45) NULL,
+     precio INT NULL,
+     destacado TINYINT(1) NULL,
+     PRIMARY KEY (idproducto))";
 
-
-   -- -----------------------------------------------------
-   -- Table `mydb`.`usuario`
-   -- -----------------------------------------------------
-   CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
-     `idusuario` VARCHAR(15) NOT NULL,
-     `contraseña` VARCHAR(20) NULL,
-     `email` VARCHAR(45) NULL,
-     `direccion` VARCHAR(45) NULL,
-     `nombre` VARCHAR(45) NULL,
-     PRIMARY KEY (`idusuario`))
-   ENGINE = InnoDB;
-
-
-   -- -----------------------------------------------------
-   -- Table `mydb`.`valoracion`
-   -- -----------------------------------------------------
-   CREATE TABLE IF NOT EXISTS `mydb`.`valoracion` (
-     `idprod` INT NOT NULL,
-     `puntuacion` VARCHAR(45) NULL,
-     `idusr` VARCHAR(45) NOT NULL,
-     PRIMARY KEY (`idprod`, `idusr`),
-     INDEX `idVal_Usr_idx` (`idusr` ASC),
-     CONSTRAINT `idVal_Prod`
-       FOREIGN KEY (`idprod`)
-       REFERENCES `mydb`.`productos` (`idproducto`)
-       ON DELETE NO ACTION
-       ON UPDATE NO ACTION,
-     CONSTRAINT `idVal_Usr`
-       FOREIGN KEY (`idusr`)
-       REFERENCES `mydb`.`usuario` (`idusuario`)
-       ON DELETE NO ACTION
-       ON UPDATE NO ACTION)
-   ENGINE = InnoDB;
-
-
-   -- -----------------------------------------------------
-   -- Table `mydb`.`comentario`
-   -- -----------------------------------------------------
-   CREATE TABLE IF NOT EXISTS `mydb`.`comentario` (
-     `idprod` INT NOT NULL,
-     `idusuario` VARCHAR(15) NOT NULL,
-     `Contenido` VARCHAR(140) NULL,
-     PRIMARY KEY (`idprod`, `idusuario`),
-     INDEX `idCom_Usr_idx` (`idusuario` ASC),
-     CONSTRAINT `idCom_Prod`
-       FOREIGN KEY (`idprod`)
-       REFERENCES `mydb`.`productos` (`idproducto`)
-       ON DELETE NO ACTION
-       ON UPDATE NO ACTION,
-     CONSTRAINT `idCom_Usr`
-       FOREIGN KEY (`idusuario`)
-       REFERENCES `mydb`.`usuario` (`idusuario`)
-       ON DELETE NO ACTION
-       ON UPDATE NO ACTION)
-   ENGINE = InnoDB;
-
-
-   -- -----------------------------------------------------
-   -- Table `mydb`.`favoritos`
-   -- -----------------------------------------------------
-   CREATE TABLE IF NOT EXISTS `mydb`.`favoritos` (
-     `idusuario` VARCHAR(15) NOT NULL,
-     `idproducto` INT NOT NULL,
-     PRIMARY KEY (`idusuario`, `idproducto`),
-     INDEX `FK_favoritos_productos_idx` (`idproducto` ASC),
-     CONSTRAINT `FK_favoritos_usuario`
-       FOREIGN KEY (`idusuario`)
-       REFERENCES `mydb`.`usuario` (`idusuario`)
-       ON DELETE NO ACTION
-       ON UPDATE NO ACTION,
-     CONSTRAINT `FK_favoritos_productos`
-       FOREIGN KEY (`idproducto`)
-       REFERENCES `mydb`.`productos` (`idproducto`)
-       ON DELETE NO ACTION
-       ON UPDATE NO ACTION)
-   ENGINE = InnoDB;";
    if ($conn->query($sql) === TRUE) {
-      echo "Tabla creada correctamente";
+      echo "Tabla productos correctamente <br>";
    } else {
-      echo "Error creando tabla: " . $conn->error;
+      echo "Error creando tabla productos: " . $conn->error."<br>";
+   }
+   $sql="-- -----------------------------------------------------
+   -- Table usuario
+   -- -----------------------------------------------------
+   CREATE TABLE IF NOT EXISTS usuario (
+     idusuario VARCHAR(15) NOT NULL,
+     password VARCHAR(20) NULL,
+     email VARCHAR(45) NULL,
+     direccion VARCHAR(45) NULL,
+     nombre VARCHAR(45) NULL,
+     PRIMARY KEY (idusuario))";
+     if ($conn->query($sql) === TRUE) {
+        echo "Tabla usuario correctamente <br>";
+     } else {
+        echo "Error creando tabla usuario: " . $conn->error."<br>";
+     }
+   $sql="-- -----------------------------------------------------
+   -- Table valoracion
+   -- -----------------------------------------------------
+   CREATE TABLE IF NOT EXISTS valoracion (
+     idprod INT NOT NULL,
+     puntuacion VARCHAR(45) NULL,
+     idusr VARCHAR(45) NOT NULL,
+     PRIMARY KEY (idprod, idusr),
+     INDEX idVal_Usr_idx (idusr ASC),
+     CONSTRAINT idVal_Prod
+       FOREIGN KEY (idprod)
+       REFERENCES productos (idproducto)
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT idVal_Usr
+       FOREIGN KEY (idusr)
+       REFERENCES usuario (idusuario)
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION)";
+   if ($conn->query($sql) === TRUE) {
+      echo "Tabla valoracion correctamente <br>";
+   } else {
+      echo "Error creando tabla valoracion: " . $conn->error."<br>";
+   }
+   $sql="-- -----------------------------------------------------
+   -- Table comentario
+   -- -----------------------------------------------------
+   CREATE TABLE IF NOT EXISTS comentario (
+     idprod INT NOT NULL,
+     idusuario VARCHAR(15) NOT NULL,
+     Contenido VARCHAR(140) NULL,
+     PRIMARY KEY (idprod, idusuario),
+     INDEX idCom_Usr_idx (idusuario ASC),
+     CONSTRAINT idCom_Prod
+       FOREIGN KEY (idprod)
+       REFERENCES productos (idproducto)
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT idCom_Usr
+       FOREIGN KEY (idusuario)
+       REFERENCES usuario (idusuario)
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION)";
+   if ($conn->query($sql) === TRUE) {
+      echo "Tabla comentario correctamente <br>";
+   } else {
+      echo "Error creando tabla comentario: " . $conn->error."<br>";
+   }
+   $sql="-- -----------------------------------------------------
+   -- Table favoritos
+   -- -----------------------------------------------------
+   CREATE TABLE IF NOT EXISTS favoritos (
+     idusuario VARCHAR(15) NOT NULL,
+     idproducto INT NOT NULL,
+     PRIMARY KEY (idusuario, idproducto),
+     INDEX FK_favoritos_productos_idx (idproducto ASC),
+     CONSTRAINT FK_favoritos_usuario
+       FOREIGN KEY (idusuario)
+       REFERENCES usuario (idusuario)
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION,
+     CONSTRAINT FK_favoritos_productos
+       FOREIGN KEY (idproducto)
+       REFERENCES productos (idproducto)
+       ON DELETE NO ACTION
+       ON UPDATE NO ACTION)";
+   if ($conn->query($sql) === TRUE) {
+      echo "Tabla favoritos correctamente <br>";
+   } else {
+      echo "Error creando tabla favoritos: " . $conn->error."<br>";
    }
  ?>
