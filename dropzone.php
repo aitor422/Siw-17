b1eefb15af6deed56e51a35d030adda91c70b733
MikelArdanaz <mikelillo_1@msn.com>
2017-05-10 12:58:03 +0200

    Dropzone sigue sin ir

--- dropzone.php
+++ dropzone.php
@@ -1,7 +1,14 @@
 <?php
-$target_dir = "static/images/catalogo/";
-$target_file = $target_dir . basename($_FILES["file"]["name"]);
-if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir.$_FILES['file']['name'])) {
-   $status = 1;
+if(isset($_FILES["file"])){
+   echo "FUNCIONA";
+   $target_dir = "static/images/catalogo/";
+   $target_file = $target_dir . basename($_FILES["file"]["name"]);
+   if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir.$_FILES['file']['name'])) {
+      echo "El fichero ". basename( $_FILES["file"]["name"]). " ha sido subido.";
+   } else {
+      echo "Error en la subida";
+   }
+}else{
+   echo "NO FUNCIONA";
 }
 ?>
