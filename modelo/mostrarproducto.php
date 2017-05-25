30c8cffa172579120a2604f71cd246dadd3f5d08
Aitor <chorda.106011@e.unavarra.es>
2017-05-25 16:44:49 +0200

    más codificación

--- modelo/mostrarproducto.php
+++ modelo/mostrarproducto.php
@@ -1,6 +1,7 @@
 <?php
 function mMostrarProducto($producto){
    $con = new mysqli("dbserver", "siw14", "eeshaekaip", "db_siw14");
+   mysqli_set_charset($con,"utf8");
    $consulta = "SELECT final_productos.idproducto, nombre, precio,descripcion  FROM final_productos WHERE final_productos.idproducto = $producto";
    $resultado = $con->query($consulta);
    return $resultado;
